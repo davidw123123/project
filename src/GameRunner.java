c8d6e8eb5b57f7058aafcc267017f39a7ad4b4bb
BT_2S14_19 <davidwg1210@gmail.com>
2024-11-25 10:41:02 -0500

    added comments so its easier to understand

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -5,19 +5,22 @@ public class GameRunner {
         Scanner s = new Scanner(System.in);
         Game user = new Game();
         Game computer = new Game();
+
         // variables needed
         int computerInitial = computer.total;
         boolean userMove = true;
         boolean computerMove = true;
         int count = 0;
+
         //teaches user how to play
         System.out.println("You start of with a card between 1-9 and your goal is to try to reach the goal of 21. You are able to hit to get a random card or stand to skip your turn."+"\n"+"If both players stand, the game will determine the winner based on who is closer to the goal. You have ability cards that could change the goal by a certain amount."+"\n"+"Goal: 21"+"\n"+"Your ability cards: " + user.abilities+" "+user.abilityDescription(user.abilities) + "\n" + "Your number: " + user.deck+"\n"+"----------------------------------------------------");
-        //
-        while (userMove || computerMove) {
+
+        while (userMove || computerMove) {//checks if both user's move are true
             System.out.print("Choose between hit or stand or use your ability: ");
             String response = s.nextLine();
-            if ((user.abilities.length()>1)&&(!response.equalsIgnoreCase("hit")&&!response.equalsIgnoreCase("stand"))){
-                if (response.equalsIgnoreCase(user.abilities)) {
+            if ((user.abilities.length()>1)&&(!response.equalsIgnoreCase("hit")&&!response.equalsIgnoreCase("stand"))){// checks if user's response is not hit nor stand and they have an ability
+
+                if (response.equalsIgnoreCase(user.abilities)) {//if response is equal to their ability nad if not, asks user to choose between hit or stand
                     System.out.println(user.useAbility(user.abilities));
                     // change the computer goal to equal the player goal
                     computer.changeGoal(user.getGoal());
@@ -26,25 +29,33 @@ public class GameRunner {
                     response = s.nextLine();
                 }
 
-            } else if (!response.equalsIgnoreCase("hit")&&!response.equalsIgnoreCase("stand")){
+            } else if (!response.equalsIgnoreCase("hit")&&!response.equalsIgnoreCase("stand")){// if user
                 System.out.println("You don't have any abilities! Choose between hit or stand");
                 response = s.nextLine();
             }
-            if ( response.equalsIgnoreCase("hit")) {
+
+            if ( response.equalsIgnoreCase("hit"))
+            {// calls hit method if response is equal to hit and stand otherwise
                 System.out.println(user.hit());
-            } else if (response.equalsIgnoreCase("stand")){
+            }
+            else if (response.equalsIgnoreCase("stand"))
+            {
                 System.out.println("Total: "+ user.total+"\n"+"----------------------------------------------------");
                 userMove = false;
             }
-            if (computerMove && count != 0 && (count % 2 == 0) && computer.abilities.length() > 1){
+
+            if (computerMove && count != 0 && (count % 2 == 0) && computer.abilities.length() > 1)
+            { //computer uses ability after 2 turns
                 System.out.println("Computer used: "+computer.abilities);
                 System.out.println(computer.useAbility(computer.abilities));
                 user.changeGoal(computer.getGoal());
                 computer.abilities = "";
                 computerMove = false;
             }
-            if (computerMove) {
-                if (((Math.abs(computer.total - computer.goal) < 5) || (Math.abs(computer.total - computer.goal) > computer.goal))) {
+
+            if (computerMove)
+            {
+                if (((Math.abs(computer.total - computer.goal) < 5) || (Math.abs(computer.total - computer.goal) > computer.goal))) { // if computer total's difference from goal is less than 5, it'll stand
                     System.out.println("Computer's Turn: Stand"+"\n" + "----------------------------------------------------");
                     computerMove = false;
                     count++;
@@ -56,7 +67,8 @@ public class GameRunner {
                 }
             }
             // Allows the user or computer to choose if they want to hit or stand again by setting their move back to true only if one of their move is true
-            if (userMove || computerMove){
+            if (userMove || computerMove)
+            {
                 userMove = true;
                 computerMove = true;
             }
